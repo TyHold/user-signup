5790c80f26785bf87f89586c51f566822ff8fb4f
Tyler Holdener <tjh112898@gmail.com>
2019-05-10 13:32:27 -0500

    added regex

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect
+import re
 app= Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -17,6 +18,8 @@ def index():
         verify_err = ''
         email_err = ''
         
+        EMAIL_REGEX = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+
         if not username or len(username) > 20 or len(username) < 3:
                 username_err = "invalid username"
         if not password or len(password) > 20 or len(password) < 3:
@@ -24,7 +27,7 @@ def index():
         if password != verify:
             verify_err = "passwords do not match"
         if email:
-            if "@" not in email or "." not in email or len(email) > 20 or len(email) < 3 or " " in email:
+            if not EMAIL_REGEX.match(email) or len(email) > 20 or len(email) < 3 or " " in email:
                 email_err = "invalid email"
         if username_err or password_err or verify_err or email_err:
             return render_template("signup.html", username_err=username_err, password_err=password_err, verify_err=verify_err, email_err=email_err)
